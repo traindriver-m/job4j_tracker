97b14f9e1da670810295086f828d2087f0a80827
Traindriver-M <sergioparovozov@mail.ru>
2021-07-29 14:27:28 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        load = another.load + load;
+        another.load = 0;
+    }
+}
