2742f28f6d059704cae6e978f6452dde31b920db
Traindriver-M <sergioparovozov@mail.ru>
2021-07-29 13:05:02 +0300

    1.3. Вызов метода с аргументами. [#166086 #183073]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -3,18 +3,18 @@ package ru.job4j.oop;
 public class Jukebox {
     public void music(int position) {
         if (position == 1) {
-            System.out.println("Пусть бегут неуклюже\n"
-                    + "Пешеходы по лужам,\n"
-                    + "А вода по асфальту рекой.\n"
-                    + "И неясно прохожим\n"
-                    + "В этот день непогожий,\n"
-                    + "Почему я веселый такой?");
+            System.out.println("Пусть бегут неуклюже"
+                    + System.lineSeparator() + "Пешеходы по лужам,"
+                    + System.lineSeparator() + "А вода по асфальту рекой."
+                    + System.lineSeparator() + "И неясно прохожим"
+                    + System.lineSeparator() + "В этот день непогожий,"
+                    + System.lineSeparator() + "Почему я веселый такой?");
         } else if (position == 2) {
-            System.out.println("Спят усталые игрушки, книжки спят.\n"
-                    + "Одеяла и подушки ждут ребят.\n"
-                    + "Даже сказка спать ложится,\n"
-                    + "Что бы ночью нам присниться.\n"
-                    + "Ты ей пожелай:\n"
+            System.out.println("Спят усталые игрушки, книжки спят."
+                    + System.lineSeparator() + "Одеяла и подушки ждут ребят."
+                    + System.lineSeparator() + "Даже сказка спать ложится,"
+                    + System.lineSeparator() + "Что бы ночью нам присниться."
+                    + System.lineSeparator() + "Ты ей пожелай:"
                     + "Баю-бай.");
         } else {
             System.out.println("Песня не найдена.");
