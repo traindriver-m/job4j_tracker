636c7457094ffa9096220498fc76832c708bdb7e
Traindriver-M <sergioparovozov@mail.ru>
2021-08-16 22:04:07 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ex/Fact.java
@@ -0,0 +1,19 @@
+package ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("The number cannot be less than zero.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
