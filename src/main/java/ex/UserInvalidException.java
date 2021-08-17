d2baee6e4cf7995825c92d003991f385d0a2d885
Traindriver-M <sergioparovozov@mail.ru>
2021-08-17 23:31:05 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ src/main/java/ex/UserInvalidException.java
@@ -0,0 +1,7 @@
+package ex;
+
+public class UserInvalidException extends UserNotFoundException {
+    public UserInvalidException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ex/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ex;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
