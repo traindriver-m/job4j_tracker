8e9e841453e14926880421b7788c3db604df8632
Traindriver-M <sergioparovozov@mail.ru>
2021-08-18 12:21:46 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #191193]

--- src/main/java/ex/UserStore.java
+++ src/main/java/ex/UserStore.java
@@ -12,11 +12,10 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (user.isValid() && user.getUsername().length() >= 3) {
-            return true;
-        } else {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("User is not valid");
         }
+        return true;
     }
 
     public static void main(String[] args) {
