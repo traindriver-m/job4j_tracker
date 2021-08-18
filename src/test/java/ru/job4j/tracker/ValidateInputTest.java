bedd8de0542fa3e134d509722513e89aad9f243c
Traindriver-M <sergioparovozov@mail.ru>
2021-08-18 17:44:22 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117 #191353]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+    }
+}
