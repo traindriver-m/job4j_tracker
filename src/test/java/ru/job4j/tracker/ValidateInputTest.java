6d68cf9456d96541cbd9c5548b91be151c58bf75
Traindriver-M <sergioparovozov@mail.ru>
2021-08-21 23:03:56 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import static org.junit.Assert.*;
 
@@ -8,5 +13,49 @@ public class ValidateInputTest {
 
     @Test
     public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Select:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Select:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenManyValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"1", "2"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Select: ");
+        assertThat(selected, is(1));
+        selected = input.askInt("Select: ");
+        assertThat(selected, is(2));
+    }
+
+    @Test
+    public void whenNegativeNumber() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Input in = new StubInput(new String[]{"-3", "0"});
+        ValidateInput input = new ValidateInput(out, in);
+        UserAction[] action = new UserAction[]{new ExitAction()};
+        new StartUI(out).init(input, tracker, action);
+        String ln = System.lineSeparator();
+        Assert.assertThat(out.toString(), is("Menu." + ln
+                + "0. Exit Program" + ln
+                + "Wrong input, you can select: 0 .. 0" + ln
+                + "Menu." + ln
+                + "0. Exit Program" + ln));
+
     }
 }
