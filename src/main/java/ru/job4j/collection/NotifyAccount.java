b1729fb0920eed8aa99b4fd4851b3bf6fba879bb
Traindriver-M <sergioparovozov@mail.ru>
2021-09-01 19:27:27 +0300

    3. Модели данных и HashSet. [#218530]

--- src/main/java/ru/job4j/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
         HashSet<Account> rsl = new HashSet<>();
-        /* for-each accounts to HashSet; */
         for (Account account : accounts) {
             rsl.add(account);
         }
