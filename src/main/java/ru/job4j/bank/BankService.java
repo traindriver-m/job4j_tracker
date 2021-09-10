3f1d93d6f2a75de7683e8672b1b01d43e4cfcf41
Traindriver-M <sergioparovozov@mail.ru>
2021-09-11 00:07:55 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,52 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        List<Account> tempList = users.get(findByPassport(passport));
+        if (!tempList.contains(account)) {
+            tempList.add(account);
+            users.put(findByPassport(passport), tempList);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        List<Account> tempAccount = users.get(findByPassport(passport));
+        if (tempAccount != null) {
+            for (Account acc : tempAccount) {
+                if (acc.getRequisite().equals(requisite)) {
+                    return acc;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            destAccount.setBalance(amount + destAccount.getBalance());
+            rsl = true;
+        }
+        return rsl;
+    }
+}
