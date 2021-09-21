19ebba1d1e701085455e2f96093a001cb1768fb2
Traindriver-M <sergioparovozov@mail.ru>
2021-09-21 14:51:59 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.function;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+    }
+}
+
