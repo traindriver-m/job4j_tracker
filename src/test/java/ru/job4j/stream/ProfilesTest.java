8b3e1d243e223e8b72a3347a3eacae2405aef82d
Traindriver-M <sergioparovozov@mail.ru>
2021-09-29 00:29:43 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -12,13 +12,14 @@ import static org.junit.Assert.*;
 public class ProfilesTest {
 
     @Test
-    public void collect() {
+    public void collectAndSorted() {
         List<Address> expected = List.of(
-                new Address("Moscow", "Kluchevaya", 8, 212),
+                new Address("Kaluga", "Voskresenskaya", 14, 24),
                 new Address("Kozelsk", "Krupskoy", 18, 1),
+                new Address("Moscow", "Kluchevaya", 8, 212),
                 new Address("Sharya", "Lenina", 95, 3),
-                new Address("Tula", "Mosina", 29, 14),
-                new Address("Kaluga", "Voskresenskaya", 14, 24)
+                new Address("Tula", "Mosina", 29, 14)
+
         );
         List<Profile> profilesList = List.of(
                 new Profile(new Address("Moscow", "Kluchevaya", 8, 212)),
@@ -26,6 +27,30 @@ public class ProfilesTest {
                 new Profile(new Address("Sharya", "Lenina", 95, 3)),
                 new Profile(new Address("Tula", "Mosina", 29, 14)),
                 new Profile(new Address("Kaluga", "Voskresenskaya", 14, 24))
+        );
+        Profiles profiles = new Profiles();
+        List<Address> rsl = profiles.collect(profilesList);
+        assertEquals(rsl, expected);
+    }
+
+    @Test
+    public void collectAndUnicue() {
+        List<Address> expected = List.of(
+                new Address("Kaluga", "Voskresenskaya", 14, 24),
+                new Address("Kozelsk", "Krupskoy", 18, 1),
+                new Address("Moscow", "Kluchevaya", 8, 212),
+                new Address("Sharya", "Lenina", 95, 3),
+                new Address("Tula", "Mosina", 29, 14)
+
+        );
+        List<Profile> profilesList = List.of(
+                new Profile(new Address("Moscow", "Kluchevaya", 8, 212)),
+                new Profile(new Address("Kozelsk", "Krupskoy", 18, 1)),
+                new Profile(new Address("Sharya", "Lenina", 95, 3)),
+                new Profile(new Address("Tula", "Mosina", 29, 14)),
+                new Profile(new Address("Kozelsk", "Krupskoy", 18, 1)),
+                new Profile(new Address("Kaluga", "Voskresenskaya", 14, 24)),
+                new Profile(new Address("Moscow", "Kluchevaya", 8, 212))
         );
         Profiles profiles = new Profiles();
         List<Address> rsl = profiles.collect(profilesList);
