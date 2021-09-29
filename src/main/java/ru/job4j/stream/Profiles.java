417b07ff51bd96a45ef40a19d9c29d6e35895a86
Traindriver-M <sergioparovozov@mail.ru>
2021-09-29 13:47:24 +0300

    2. Список адресов [#110062 #209846]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,7 +6,8 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress)
+        return profiles.stream()
+                .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
