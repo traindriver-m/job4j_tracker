0d0ace9cea49dfe739df9429b255398a82c7606d
Traindriver-M <sergioparovozov@mail.ru>
2021-10-04 14:35:35 +0300

    4. Optional в Stream API. [#279130 #210997]

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.bank;
 
 import org.junit.Test;
+
+import java.util.Optional;
+
 import static org.hamcrest.core.Is.is;
 
 import static org.junit.Assert.*;
@@ -20,7 +23,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertNull(bank.findByRequisite("34", "5546"));
+        assertThat(bank.findByRequisite("34", "5546"), is(Optional.empty()));
     }
 
     @Test
